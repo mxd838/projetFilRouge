c237c7ce7d72a0d727ccbb061dc28d56cabf6652
Gabi <gabriel.delachat@gmail.com>
2022-07-22 10:28:22 +0200

    footer2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Footer from './component/Footer';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
